4c97bbf8104e687d234e86d59123c3ba1b31ecf9
Danilo <dlb__@hotmail.com>
2016-10-22 20:06:01 +0100

    First flask app

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,17 +6,33 @@ from flask import Flask
 #create application object
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
+
 #use decorators to link the function to a url
 
-@app.route("/")
-@app.route("/hello")
 
+
+@app.route("/integer/<int:value>")
 #define the view using a function, wich returns a string
-def hello_world():
-	return "Hello, World!"
+def int_type(value):
+	print value + 1
+	return "correct"
+	
 
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello!?!?!?!, {}".format(name), 200
+	else:
+	    return "Not Found", 404	
 #start the development server using the run() method
 if __name__ == "__main__":
 	app.run()
 
-	
+
